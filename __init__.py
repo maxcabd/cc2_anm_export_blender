4a773d5330c1a3ca2fb466eb25cb01c8d6582273
maxcabd <84046498+maxcabd@users.noreply.github.com>
2025-02-18 15:57:34 -0500

    Update versioning

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ blender_loader = importlib.util.find_spec('bpy')
 bl_info = {
     "name": "CyberConnect2 ANM XFBIN File Export",
     "author": "Dei, TheLeonX, Al-Hydra",
-    "version": (1, 1, 0),
+    "version": (1, 0, 5),
     "blender": (4, 2, 0),
     "location": "File > Export",
     "description": "Export XFBIN animation files found in CyberConnect2 Naruto Storm and JoJo games.",
